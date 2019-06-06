d5f709665e7afd462b4cd10d8ed5f6abe04340b5
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-06-06 04:31:14 -0500

    unsubscribe the subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 import { ThemeService } from './core/services/theme.service';
 import { WeatherService } from 'src/services/weather.service';
@@ -9,25 +9,35 @@ import { WeatherService } from 'src/services/weather.service';
   templateUrl:'./app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy{
   isDarkTheme: Observable<boolean>;
   zipcode:string='77058';
   public weatherData;
   public forecastData;
+  private subscriptions: Subscription[] = [];
 
 
   constructor(private themeService: ThemeService, private weatherService: WeatherService) { }
 
   ngOnInit() {
     this.isDarkTheme = this.themeService.isDarkTheme;
-    this.weatherService.getWeather(this.zipcode).subscribe(res => {console.log('wres', res);this.weatherData=res; });
-    this.weatherService.getWeatherForcast(this.zipcode).subscribe(res => {console.log('fres', res);this.forecastData=res; });
+    const sub1 = this.weatherService.getWeather(this.zipcode).subscribe(res => {console.log('wres', res);this.weatherData=res; });
+    const sub2 = this.weatherService.getWeatherForcast(this.zipcode).subscribe(res => {console.log('fres', res);this.forecastData=res; });
+    this.subscriptions.push(sub1); this.subscriptions.push(sub2);
+
   }
 
   onSubmitZip(zipcd){
      this.zipcode=zipcd.zipcd;
-     this.weatherService.getWeather(this.zipcode).subscribe(res => {console.log('wres', res);this.weatherData=res; });
-     this.weatherService.getWeatherForcast(this.zipcode).subscribe(res => {console.log('fres', res);this.forecastData=res; });
+     const sub1 = this.weatherService.getWeather(this.zipcode).subscribe(res => {console.log('wres', res);this.weatherData=res; });
+     const sub2 = this.weatherService.getWeatherForcast(this.zipcode).subscribe(res => {console.log('fres', res);this.forecastData=res; });
+     this.subscriptions.push(sub1); this.subscriptions.push(sub2);
     //call to server will happen only on subscribe
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 }
