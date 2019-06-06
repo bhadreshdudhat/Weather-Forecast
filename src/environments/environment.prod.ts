486667dbcc581d37d78315a79354049e04023f37
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-06-06 11:25:22 -0500

    Deployed on Azure cloud using VSCode azure extention

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   weatherUrl: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather',
-  forecastUrl: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast',
-  iconUrl: 'https://openweathermap.org/img/w'
+  forecastUrl: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast'
 };
